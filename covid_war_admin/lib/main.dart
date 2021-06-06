fb21b72041db2c637a75a7b164fa8bc8a01cbe37
RamNarayan27 <ramnarayanbahadur@gmail.com>
2021-06-06 19:25:04 +0530

    Delete entry done
    Selected Hospital entries get deleted.

--- covid_war_admin/lib/main.dart
+++ covid_war_admin/lib/main.dart
@@ -1,17 +1,11 @@
-// todo : finish editform
-// todo : get the values from there and update it in the listview
-// todo : finish editform
-// todo : get the values and create a new list entry
-// todo : add a search functionality based on hospital and all
-
-// todo : set up firebase
+// todo : add a search functionality based on hospital
 
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'editform.dart';
 import 'newentry.dart';
+import 'dart:developer' as developer;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -85,6 +79,9 @@ class _MyStatefulWidget extends State<MyStatefulWidget> {
   // final entries = <String>['Apollo', 'Billroth', 'Jaruko'];
   final entries2 = <String>['dlsfjasdf', 'adfasfasfda', 'asdlfhas;dfasf'];
   final List<int> colorCodes2 = <int>[600, 500, 100];
+  List<int> selectedItems = [];
+  bool switcher = false;
+  PreferredSizeWidget appbar = AppBar(title: Text('Covid War Room'));
 
   Widget _buildList() {
     if (_selectedIndex == 0) {
@@ -100,106 +97,228 @@ class _MyStatefulWidget extends State<MyStatefulWidget> {
               padding: const EdgeInsets.all(0),
               itemCount: snapshot.data.size,
               itemBuilder: (BuildContext context, int index) {
-                return ExpansionTile(
-                  leading: Icon(
-                    Icons.local_hospital_rounded,
-                    color: Colors.red,
-                    size: 56.0,
-                  ),
-                  title: Text(snapshot.data.docs[index]['Hospital Name']),
-                  subtitle: Text(
-                      '# Total Beds : ${snapshot.data.docs[index]['total-beds']}'),
-                  trailing: Icon(Icons.arrow_drop_down_rounded),
-                  children: [
-                    Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 50, vertical: 4),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('Hospital Name'),
-                          Text('${snapshot.data.docs[index]['Hospital Name']}')
-                        ],
-                      ),
-                    ),
-                    Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 50, vertical: 4),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('# Total Beds'),
-                          Text('${snapshot.data.docs[index]['total-beds']}')
-                        ],
-                      ),
-                    ),
-                    Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 50, vertical: 4),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('# ICU'),
-                          Text('${snapshot.data.docs[index]['icu']}')
-                        ],
-                      ),
-                    ),
-                    Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 50, vertical: 4),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('# Ventilator'),
-                          Text('${snapshot.data.docs[index]['ventilator']}')
-                        ],
-                      ),
-                    ),
-                    Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 50, vertical: 4),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('# O2 Beds'),
-                          Text('${snapshot.data.docs[index]['oxy-beds']}')
-                        ],
-                      ),
-                    ),
-                    Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 50, vertical: 4),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('# Normal Beds'),
-                          Text('${snapshot.data.docs[index]['normal-beds']}')
-                        ],
-                      ),
-                    ),
-                    Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 50, vertical: 4),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          ElevatedButton(
-                            child: Text('Edit'),
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => EditFormPage(),
-                                      settings: RouteSettings(
-                                        arguments: HospitalData.fromJSON(
-                                            snapshot.data.docs[index]),
-                                      )));
-                            },
+                return InkWell(
+                  onLongPress: () {
+                    // add the widget to the list here
+                    setState(() {
+                      selectedItems.add(index);
+                      switcher = true;
+                      if (switcher) {
+                        appbar = AppBar(
+                          title: Text('Delete Hospitals'),
+                          actions: [
+                            IconButton(
+                              icon: Icon(Icons.delete),
+                              onPressed: () {
+                                //delete firebase entry here
+                                selectedItems.forEach((element) async {
+                                  await FirebaseFirestore.instance
+                                      .runTransaction(
+                                          (Transaction myTransaction) async {
+                                    await myTransaction.delete(
+                                        snapshot.data.docs[element].reference);
+                                  });
+                                  // final snackBar =
+                                  //     SnackBar(content: Text('${element}'));
+                                  // ScaffoldMessenger.of(context)
+                                  //     .showSnackBar(snackBar);
+                                  developer.log(element.toString(),
+                                      name: 'deleted entry');
+                                });
+                                setState(() {
+                                    switcher = false;
+                                    selectedItems.clear();
+                                    appbar =
+                                        AppBar(title: Text('Covid War Room'));
+                                  });
+                              },
+                            )
+                          ],
+                        );
+                      }
+                    });
+                    // final snackBar =
+                    //     SnackBar(content: Text('Yay! A SnackBar!'));
+                    // ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    developer.log(selectedItems.toString(),
+                        name: 'onlongpress works');
+                  },
+                  child: (switcher)
+                      ? (selectedItems.contains(index))
+                          ? ListTileTheme(
+                              // represents the list that is present in the list
+                              // ontap should remove the goddamn thing from the list
+                              tileColor: Colors.black12,
+                              child: ListTile(
+                                leading: Icon(
+                                  Icons.local_hospital_rounded,
+                                  color: Colors.red,
+                                  size: 56.0,
+                                ),
+                                title: Text(
+                                    snapshot.data.docs[index]['Hospital Name']),
+                                subtitle: Text(
+                                    '# Total Beds : ${snapshot.data.docs[index]['total-beds']}'),
+                                onTap: () {
+                                  setState(() {
+                                    selectedItems.remove(index);
+                                    // final snackBar = SnackBar(
+                                    //     content: Text('Yay! A SnackBar!'));
+                                    // ScaffoldMessenger.of(context)
+                                    //     .showSnackBar(snackBar);
+                                    developer.log(selectedItems.toString(),
+                                        name: 'ontap remove works');
+                                    if (selectedItems.isEmpty) {
+                                      switcher = false;
+                                      appbar =
+                                          AppBar(title: Text('Covid War Room'));
+                                    }
+                                  });
+                                },
+                              ),
+                            )
+                          : ListTileTheme(
+                              // represents the list that is not present
+                              // ontap should add the index into the list
+                              child: ListTile(
+                                leading: Icon(
+                                  Icons.local_hospital_rounded,
+                                  color: Colors.red,
+                                  size: 56.0,
+                                ),
+                                title: Text(
+                                    snapshot.data.docs[index]['Hospital Name']),
+                                subtitle: Text(
+                                    '# Total Beds : ${snapshot.data.docs[index]['total-beds']}'),
+                                onTap: () {
+                                  setState(() {
+                                    selectedItems.add(index);
+                                    // final snackBar = SnackBar(
+                                    //     content: Text('Yay! A SnackBar!'));
+                                    // ScaffoldMessenger.of(context)
+                                    //     .showSnackBar(snackBar);
+                                    developer.log(selectedItems.toString(),
+                                        name: 'ontap add works');
+                                  });
+                                },
+                              ),
+                            )
+                      : ExpansionTile(
+                          leading: Icon(
+                            Icons.local_hospital_rounded,
+                            color: Colors.red,
+                            size: 56.0,
                           ),
-                        ],
-                      ),
-                    ),
-                  ],
+                          title:
+                              Text(snapshot.data.docs[index]['Hospital Name']),
+                          subtitle: Text(
+                              '# Total Beds : ${snapshot.data.docs[index]['total-beds']}'),
+                          trailing: Icon(Icons.arrow_drop_down_rounded),
+                          children: [
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 50, vertical: 4),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text('Hospital Name'),
+                                  Text(
+                                      '${snapshot.data.docs[index]['Hospital Name']}')
+                                ],
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 50, vertical: 4),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text('# Total Beds'),
+                                  Text(
+                                      '${snapshot.data.docs[index]['total-beds']}')
+                                ],
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 50, vertical: 4),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text('# ICU'),
+                                  Text('${snapshot.data.docs[index]['icu']}')
+                                ],
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 50, vertical: 4),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text('# Ventilator'),
+                                  Text(
+                                      '${snapshot.data.docs[index]['ventilator']}')
+                                ],
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 50, vertical: 4),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text('# O2 Beds'),
+                                  Text(
+                                      '${snapshot.data.docs[index]['oxy-beds']}')
+                                ],
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 50, vertical: 4),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text('# Normal Beds'),
+                                  Text(
+                                      '${snapshot.data.docs[index]['normal-beds']}')
+                                ],
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 50, vertical: 4),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  ElevatedButton(
+                                    child: Text('Edit'),
+                                    onPressed: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  EditFormPage(),
+                                              settings: RouteSettings(
+                                                arguments:
+                                                    HospitalData.fromJSON(
+                                                        snapshot
+                                                            .data.docs[index]),
+                                              )));
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                 );
               });
         },
@@ -229,6 +348,8 @@ class _MyStatefulWidget extends State<MyStatefulWidget> {
     setState(() {
       if (_selectedIndex == 0) {
         // todo : add a new route here to add a new value
+        Navigator.push(
+            context, MaterialPageRoute(builder: (content) => NewEntry()));
       } else {
         entries2.add('Fire');
         colorCodes2.add(400);
@@ -239,26 +360,13 @@ class _MyStatefulWidget extends State<MyStatefulWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Covid War Room'),
-      ),
+      appBar: appbar,
       body: Center(
         child: _buildList(),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // ! firebase will update it here
-          Fluttertoast.showToast(
-              msg: 'Test toast for fab',
-              toastLength: Toast.LENGTH_SHORT,
-              gravity: ToastGravity.CENTER,
-              timeInSecForIosWeb: 1,
-              backgroundColor: Colors.grey,
-              textColor: Colors.black,
-              fontSize: 16.0);
           _getANewItem(_selectedIndex);
-          Navigator.push(
-              context, MaterialPageRoute(builder: (content) => NewEntry()));
         },
         child: const Icon(Icons.add),
       ),
